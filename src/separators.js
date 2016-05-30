a7fad53700f45a3a31a2673650721e83fb36eb53
wanpanlucky <1462607050@qq.com>
2016-05-30 18:02:54 +0800

    Update separators.js

--- src/separators.js
+++ src/separators.js
@@ -1,7 +1,21 @@
 'use strict';
 
 function thousands_separators(num) {
-  
+  var numString = num.toString(),
+	flag = numString.indexOf("."),
+	_length = numString.length,
+	_point = "",
+	_int = "";
+	if (/\d+/.test(numString) && !isNaN(num)) {
+	if (flag != -1) {
+	_point = numString.substring(flag, _length);
+	_int = numString.substring(0, flag).split('').reverse().join('').replace(/(\d{3})/g, '$1,').replace(/\,$/, '').split('').reverse().join('');
+	} 
+	else {
+	_int = numString.split('').reverse().join('').replace(/(\d{3})/g, '$1,').replace(/\,$/, '').split('').reverse().join('');
+	}
+	return num = _int + _point;
+	} 
 }
 
 module.exports = thousands_separators;
